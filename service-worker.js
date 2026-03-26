afda14894af16f326355bcb5b3766e135d762aaa
Abdur Rahman <prince.ar.abdur.rahman2008@gmail.com>
2026-03-26 08:52:46 +0600

    Final smart calculator

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,15 @@
-const CACHE_NAME = "nexvora-v2";
+const CACHE_NAME="nexvora-smart-v1";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html"
-      ]);
-    })
-  );
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open(CACHE_NAME).then(cache=>{
+   return cache.addAll(["/","/index.html"]);
+  })
+ );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
 });
